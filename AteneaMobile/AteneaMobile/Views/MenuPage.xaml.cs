60b03da365ce83b0f41ee809d03c5ff8285a376b
jonathansanguino <jonathansanguino@alu.frt.utn.edu.ar>
2018-11-18 22:16:41 -0300

    Nueva vista MasterDetail

--- AteneaMobile/AteneaMobile/Views/MenuPage.xaml.cs
+++ AteneaMobile/AteneaMobile/Views/MenuPage.xaml.cs
@@ -8,34 +8,34 @@ namespace AteneaMobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MenuPage : ContentPage
     {
-        AteneaMobile.Views.MainPage RootPage
-        {
-            get => Application.Current.MainPage as AteneaMobile.Views.MainPage;
-        }
+        //AteneaMobile.Views.MainPage RootPage
+        //{
+        //    get => Application.Current.MainPage as AteneaMobile.Views.MainPage;
+        //}
 
-        List<HomeMenuItem> menuItems;
+        //List<HomeMenuItem> menuItems;
 
         public MenuPage()
         {
             InitializeComponent();
 
-            menuItems = new List<HomeMenuItem>
-            {
-                new HomeMenuItem {Id = MenuItemType.ConsultarPaciente, Title = "Consultar Paciente"},
-                new HomeMenuItem {Id = MenuItemType.Salir, Title = "Salir"}
-            };
+            //menuItems = new List<HomeMenuItem>
+            //{
+            //    new HomeMenuItem {Id = MenuItemType.ConsultarPaciente, Title = "Consultar Paciente"},
+            //    new HomeMenuItem {Id = MenuItemType.Salir, Title = "Salir"}
+            //};
 
-            ListViewMenu.ItemsSource = menuItems;
+            //ListViewMenu.ItemsSource = menuItems;
 
-            ListViewMenu.SelectedItem = menuItems[0];
-            ListViewMenu.ItemSelected += async (sender, e) =>
-            {
-                if (e.SelectedItem == null)
-                    return;
+            //ListViewMenu.SelectedItem = menuItems[0];
+            //ListViewMenu.ItemSelected += async (sender, e) =>
+            //{
+            //    if (e.SelectedItem == null)
+            //        return;
 
-                var id = (int) ((HomeMenuItem) e.SelectedItem).Id;
-                await RootPage.NavigateFromMenu(id);
-            };
+            //    var id = (int)((HomeMenuItem)e.SelectedItem).Id;
+            //    await RootPage.NavigateFromMenu(id);
+            //};
 
         }
     }
